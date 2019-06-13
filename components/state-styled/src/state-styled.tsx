c0a72c3eda88ee2de8c93816f3e231ef465572cd
Meno Abels <meno.abels@adviser.com>
2019-06-13 09:01:42 +0200

    chore: style component cleanup

--- components/state-styled/src/state-styled.tsx
+++ components/state-styled/src/state-styled.tsx
@@ -1,3 +1,4 @@
+/** @jsx jsx */
 import * as React from 'react';
 import { jsx } from '@emotion/core';
 import { observer } from 'mobx-react';
@@ -7,13 +8,6 @@ import { AppModel } from '@skeleton-xn/models';
 import { AppModelContext } from '@skeleton-xn/app-model-context';
 import { action } from 'mobx';
 
-// export const StateStyled: React.FunctionComponent = styled(StateMobx)<StateMobxProps>`
-//   background: red;
-//   ul {
-//     background: 'blue';
-//   }
-// `;
-
 const InputWhatToSay = observer(({app}: { app: AppModel }) => {
   return <input value={app.whatToSay.get()}
     onChange={action((e: React.ChangeEvent<HTMLInputElement>) =>
@@ -21,20 +15,15 @@ const InputWhatToSay = observer(({app}: { app: AppModel }) => {
 });
 
 const LiTick = observer(({app}: { app: AppModel }) => {
-  const StyledLi = styled.li`
-    background: ${app.ticker.value % 2 ? 'red' : 'blue'}
-  `;
-  return <StyledLi>{app.ticker.value}</StyledLi>;
+  return <li css={{
+    background: app.ticker.value % 2 ? 'red' : 'blue'
+  }}>{app.ticker.value}</li>;
 });
 
 const LiWhatToSay = observer(({app}: { app: AppModel }): JSX.Element => {
-  const My: React.FunctionComponent =
-    ({className}: {className?: string, children?: React.ReactNode}) =>
-      <li className={className} key={4711}>{app.whatToSay.get()}</li>;
-  const X = styled(My)`
-    background: ${app.whatToSay.get().length % 2 ? 'green' : 'orange'}
-  `;
-  return <X />;
+  return <li css={{
+    background: app.whatToSay.get().length % 2 ? 'green' : 'orange'
+  }} key={4711}>{app.whatToSay.get()}</li>;
 });
 
 export function StateStyled(props: {}): JSX.Element {
@@ -54,50 +43,3 @@ export function StateStyled(props: {}): JSX.Element {
     </AppModelContext.Consumer>;
 
 }
-
-// extends React.Component<StateMobxProps> {
-
-//   private timer: any;
-
-//   @observable
-//   private whatToSay: string;
-//   @observable
-//   private tick: number;
-
-//   public constructor(props: StateMobxProps) {
-//     super(props);
-//     this.whatToSay = '';
-//     this.tick = 0;
-//   }
-
-//   public componentDidMount() {
-//     this.timer = setInterval(action(() => {
-//       this.tick++;
-//     }), this.props.tickFreq || 1000);
-//   }
-
-//   public componentWillUnmount() {
-//     clearInterval(this.timer);
-//   }
-
-//   @action
-//   public onChangeWhatToSay = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     // console.log('onChangeWhatToSay:', e.currentTarget.value);
-//     this.whatToSay = e.currentTarget.value;
-//   }
-
-//   public render() {
-//     return <div className={this.props.className}>
-//       <form>
-//         <fieldset>
-//           <label>What to say</label>
-//           <InputString value={this.whatToSay} onChange={this.onChangeWhatToSay} />
-//         </fieldset>
-//       </form>
-//       <ul>
-//         <LiTick value={this.tick} />
-//         <LiWhatToSay value={this.whatToSay} />
-//       </ul>
-//     </div>;
-//   }
-// }
